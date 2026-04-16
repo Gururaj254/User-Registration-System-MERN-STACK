8e8f6389a2a1ac023a7893f02c739eb362bdde2d
Gururaj254 <gururajdharmashetti390@gmail.com>
2026-04-16 13:32:56 +0530

    React Identity System implemented

--- db_user_id.sql
+++ db_user_id.sql
@@ -4,6 +4,20 @@ use identity_manager_db;
 
 select * from users;
 
+ALTER TABLE Users ADD COLUMN resetPasswordToken VARCHAR(255) NULL;
+
+ALTER TABLE Users ADD COLUMN resetPasswordExpires DATETIME NULL;
+
 UPDATE Users SET role = 'admin' WHERE email = 'your-email@gmail.com';
+
+UPDATE Users SET role = 'admin' WHERE email = 'gfg12@gmail.com';
+
+UPDATE Users SET role = 'admin' WHERE email = 'mah123@gmail.com';
+
+UPDATE Users SET role = 'admin' WHERE email = 'savitri123@gmail.com';
+
+
+
+
 
 
